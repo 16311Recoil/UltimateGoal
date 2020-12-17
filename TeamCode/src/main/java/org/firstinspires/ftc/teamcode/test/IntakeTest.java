c6c498f4acc828ae5ab334f9f5aa0848520d4acf
AdityaAngajala <aditya.angajala01@k12.leanderisd.org>
2020-12-16 19:07:46 -0600

    PID + Intake Test Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/IntakeTest.java
@@ -18,8 +18,10 @@ public class IntakeTest extends OpMode
     private NopeRope robot;
     private double power = 0.5;
     private double increment = 0.1;
+    private boolean changeB = false;
+    private boolean changeX = false;
     //private Intake intake;//do i need this if i already have NopeRope
-    private Drivetrain dt;
+    private Intake in;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -30,16 +32,10 @@ public class IntakeTest extends OpMode
 
         robot = new NopeRope(this);
         //intake = new Intake(this, null);
-        try {
-            dt = new Drivetrain(this, null);
-        } catch (InterruptedException e) {
-            telemetry.addLine("NOT WORJ");
-            telemetry.update();
-            e.printStackTrace();
-        }
+        in = new Intake(this, null);
 
         // Tell the driver that initialization is complete.
-        //telemetry.addData("Status", "Initialized");
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
 
@@ -55,18 +51,27 @@ public class IntakeTest extends OpMode
      */
     @Override
     public void start() {
-        ElapsedTime timer = new ElapsedTime();
-        double angle = 0;
 
-        while (timer.milliseconds() < 2000){
-            dt.moveTelop(power * Math.cos(angle), power * Math.sin(angle), 0);
-
-        }
     }
 
     @Override
     public void loop() {
-
+        ElapsedTime timer = new ElapsedTime();
+        double angle = 0;
+        if (gamepad1.a){
+            in.setPower(power);
+        }
+        else {
+            in.setPower(0);
+        }
+        if (gamepad1.b && !changeB){
+            power += 0.05;
+        }
+        if (gamepad1.x && !changeX){
+            power += 0.05;
+        }
+        changeB = gamepad1.b;
+        changeX = gamepad1.x;
     }
 
 
